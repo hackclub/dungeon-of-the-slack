7bb722cbda0cedd13e6a4f1705a30895cfd2fc69
Celeste Drummond <parafactual@gmail.com>
2021-11-16 13:55:15 -0600

    connect to socket

--- Slack.hs
+++ Slack.hs
@@ -8,18 +8,52 @@ module Slack
 
 import           Relude                  hiding ( error )
 
+import           Control.Concurrent             ( forkIO )
 import           Control.Lens
 import           Data.Aeson                     ( FromJSON )
+import qualified Data.Text                     as T
 import           Network.Wreq
+
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+import           Network.WebSockets
+import           Wuss
 
 
 data GetConnResponse = GetConnResponse
   { ok    :: Bool
-  , error :: Maybe String
-  , url   :: Maybe String
+  , error :: Maybe Text
+  , url   :: Maybe Text
   }
   deriving Generic
 instance FromJSON GetConnResponse
+
+
+type Parser = Parsec Void Text
+
+data URL = URL
+  { host :: Text
+  , path :: Text
+  }
+
+parseURL :: Parser URL
+parseURL = do
+  _     <- string "wss://"
+  host' <- takeWhileP Nothing (/= '/')
+  path' <- takeWhileP Nothing (const True)
+
+  return URL { host = host', path = path' }
+
+
+wsClient :: ClientApp ()
+wsClient conn = do
+  putStrLn "Connected!"
+
+  _ <- forkIO . forever $ receiveData conn >>= liftIO . putTextLn
+  let loop = do
+        loop
+  void loop
 
 
 connect :: Text -> Text -> IO ()
@@ -33,15 +67,15 @@ connect _ wsToken = do
     Nothing ->
       void
         . die
+        . toString
         . ("Failed to get WebSocket URI with: " <>)
         . fromMaybe "(no error)"
         . error
         . (^. responseBody)
         $ getConnRes
-    Just url' -> connect' url'
+    Just url' -> case runParser parseURL "" url' of
+      Left e -> void . die . ("Failed to parse URI: " <>) . show $ e
+      Right u ->
+        runSecureClient (toString . host $ u) 443 (toString . path $ u) wsClient
 
   return ()
- where
-  connect' url = do
-    -- TODO
-    undefined
