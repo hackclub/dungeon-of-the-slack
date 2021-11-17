2e41c527679658d2135dc94e46eca373667f5fcf
Celeste Drummond <parafactual@gmail.com>
2021-11-16 18:24:41 -0600

    send acknowledgements (may want to fix)

--- Main.hs
+++ Main.hs
@@ -11,11 +11,16 @@ import           Slack
 import           System.Environment
 
 
+handleMsg :: EventHandler
+handleMsg msg = do
+  putStrLn $ "Message from socket: " <> show msg
+  return "Received"
+
+
 main :: IO ()
 main = do
-  apiToken <- lookupEnv "SLACK_API_TOKEN"
-  wsToken  <- lookupEnv "SLACK_WS_TOKEN"
-  case (apiToken, wsToken) of
-    (Just at, Just wst) -> connect (fromString at) (fromString wst)
-    _                   -> putStrLn
-      "Can't find SLACK_API_TOKEN and/or SLACK_WS_TOKEN environment variables"
+  -- apiToken <- lookupEnv "SLACK_API_TOKEN"
+  wsToken <- lookupEnv "SLACK_WS_TOKEN"
+  case wsToken of
+    Just wst -> wsConnect (fromString wst) handleMsg
+    Nothing  -> putStrLn "Can't find SLACK_WS_TOKEN environment variable"
--- Slack.hs
+++ Slack.hs
@@ -4,14 +4,16 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Slack
-  ( connect
+  ( wsConnect
+  , EventHandler
+  , SocketEvent(..)
   ) where
 
 import           Relude                  hiding ( error
                                                 , many
                                                 )
 
-import           Control.Lens
+import           Control.Lens            hiding ( (.=) )
 import           Data.Aeson
 import           Network.Wreq
 
@@ -49,27 +51,58 @@ parseURL = do
   return URL { host = host', path = fromString path' }
 
 
-data SlackMessage = Hello | SlashCommand | Unknown deriving Show
+data SocketEvent = SocketEvent
+  { inEnvId :: Maybe Text
+  , content :: SocketEventContent
+  }
+  deriving Show
+data SocketEventContent = Hello | SlashCommand | Unknown deriving Show
 
-instance FromJSON SlackMessage where
+instance FromJSON SocketEvent where
   parseJSON = withObject "SlackMessage" $ \v -> do
     typeName :: Text <- v .: "type"
-    return $ case typeName of
-      "hello"          -> Hello
-      "slash_commands" -> SlashCommand
-      _                -> Unknown
+    let smContent =
+          (case typeName of
+            "hello"          -> Hello
+            "slash_commands" -> SlashCommand
+            _                -> Unknown
+          )
 
-wsClient :: ClientApp ()
-wsClient conn = do
+    envId' :: Maybe Text <- v .:? "envelope_id"
+    return $ SocketEvent envId' smContent
+
+
+data SocketEventRes = SocketEventRes
+  { outEnvId :: Text
+  , resText  :: Text
+  }
+
+instance ToJSON SocketEventRes where
+  toJSON om = object
+    ["envelope_id" .= outEnvId om, "payload" .= object ["text" .= resText om]]
+
+
+-- returns response text; this probably only applies to slash cmds
+type EventHandler = SocketEvent -> IO Text
+
+wsClient :: EventHandler -> ClientApp ()
+wsClient handleMsg conn = do
   putStrLn "Connected!"
   forever $ receiveData conn >>= \msg -> do
     liftIO $ case (decode . encodeUtf8) (msg :: Text) of
-      Nothing -> void . die $ "Failed to parse JSON"
-      Just s  -> print (s :: SlackMessage)
+      Nothing -> void . die $ "Failed to parse JSON: " <> toString msg
+      Just (se :: SocketEvent) -> do
+        case inEnvId se of
+          Nothing  -> return ()
+          Just id_ -> do
+            text <- handleMsg se
+            sendTextData
+              conn
+              (encode SocketEventRes { outEnvId = id_, resText = text })
 
 
-connect :: Text -> Text -> IO ()
-connect _ wsToken = do
+wsConnect :: Text -> EventHandler -> IO ()
+wsConnect wsToken handle = do
   getURLRes <- asJSON =<< postWith
     (defaults & header "Authorization" .~ ["Bearer " <> encodeUtf8 wsToken])
     "https://slack.com/api/apps.connections.open"
@@ -86,8 +119,8 @@ connect _ wsToken = do
         . (^. responseBody)
         $ getURLRes
     Just url' -> case runParser parseURL "" url' of
-      Left e -> void . die . ("Failed to parse URI: " <>) . show $ e
-      Right u ->
-        runSecureClient (toString . host $ u) 443 (toString . path $ u) wsClient
-
-  return ()
+      Left  e -> void . die . ("Failed to parse URI: " <>) . show $ e
+      Right u -> runSecureClient (toString . host $ u)
+                                 443
+                                 (toString . path $ u)
+                                 (wsClient handle)
